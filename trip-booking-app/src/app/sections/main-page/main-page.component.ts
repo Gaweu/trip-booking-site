1599d5b4ad74762bcae0a73601aa048a7ed12618
Gawel Kaszczynski <gawel.kaszczynski@q-perior.pl>
2023-04-21 01:54:34 +0200

    Add about me section

--- trip-booking-app/src/app/sections/main-page/main-page.component.ts
+++ trip-booking-app/src/app/sections/main-page/main-page.component.ts
@@ -12,4 +12,16 @@ export class MainPageComponent implements OnInit {
   ngOnInit() {
   }
 
+  employees: { image: string, firstName: string, lastName: string, description: string }[] = [
+    { "image": "../../../assets/employee-photos/employee1.jpg","firstName": "John", "lastName": "Billy", "description": "I like trains" },
+    { "image": "../../../assets/employee-photos/employee2.jpg","firstName": "Bob", "lastName": "Krasinski", "description": "I like trains" },
+    { "image": "../../../assets/employee-photos/employee3.jpg","firstName": "John", "lastName": "Billy", "description": "I like trains" },
+  ];
+
+  tripOffers: {image: string, tripDestination: string, tripDescription: string}[] = [
+    { "image": "../../../assets/employee-photos/employee1.jpg","tripDestination": "John", "tripDescription": "Billy" },
+    { "image": "../../../assets/employee-photos/employee2.jpg","tripDestination": "Bob", "tripDescription": "Krasinski" },
+    { "image": "../../../assets/employee-photos/employee3.jpg","tripDestination": "John", "tripDescription": "Billy" },
+  ]
+
 }
