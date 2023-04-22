dd74ab4b9efca023c556623d9023082911c1676a
Gawel Kaszczynski <gawel.kaszczynski@q-perior.pl>
2023-04-23 01:14:34 +0200

    Responsive carousel

--- trip-booking-app/src/app/sections/main-page/main-page.component.ts
+++ trip-booking-app/src/app/sections/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { single } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
@@ -9,8 +10,111 @@ export class MainPageComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild('flexContainer', { static: false }) container: ElementRef<HTMLInputElement> = {} as ElementRef;
+  @ViewChild('tripCarousel', { static: false }) tripSection: ElementRef<HTMLInputElement> = {} as ElementRef;
+
+  MIN_GAP = 10;
+  counter = 0;
+  isScaled = false
+
   ngOnInit() {
+    this.displayedTripOffers = this.tripOffers.slice(0, 1);
   }
+
+  ngAfterViewInit(): void {
+    let intervalId = setInterval(() => {
+      if (this.isScaled == false) {
+        this.updateCarousel()
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 1);
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.updateCarousel()
+  }
+
+  private updateCarousel() {
+    let containerWidth = this.container.nativeElement.offsetWidth;
+
+    let items = this.container.nativeElement.children;
+    let itemsLength = items.length;
+    let totalWidth = 0;
+    for (let i = 0; i < items.length; i++) {
+    const child = items[i] as HTMLElement;
+    totalWidth += child.offsetWidth;
+    }
+
+    let singleWidth = totalWidth/itemsLength
+
+
+    const gap = Math.floor((containerWidth % totalWidth/itemsLength) / (this.displayedTripOffers.length - 1));
+    if((gap < this.MIN_GAP) && (containerWidth < totalWidth + (itemsLength+1)*this.MIN_GAP))
+    {
+      this.displayedTripOffers = this.tripOffers.slice(this.counter, itemsLength+this.counter-1);
+      console.log(false)
+    }
+    else if((containerWidth > (totalWidth+singleWidth) + (itemsLength+2)*this.MIN_GAP))
+    {
+      if(this.tripOffers[itemsLength+this.counter])
+      {
+        this.displayedTripOffers.push(this.tripOffers[itemsLength+this.counter]);
+      }
+      else if(this.tripOffers[this.counter-1])
+      {
+        this.displayedTripOffers.unshift(this.tripOffers[this.counter-1]);
+      }
+      console.log(true)
+    }
+    else {
+      this.isScaled = true
+    }
+  }
+
+  moveNext(): void {
+    let currentArrayLength = this.displayedTripOffers.length;
+    if(!this.tripOffers[currentArrayLength + this.counter])
+    {
+      console.log(false);
+    }
+    else
+    {
+      this.displayedTripOffers.shift();
+      this.displayedTripOffers.push(this.tripOffers[currentArrayLength + this.counter]);
+      this.counter++;
+      console.log(this.counter);
+
+    }
+
+    console.log(this.displayedTripOffers);
+  }
+
+  movePrevious(): void {
+
+    if(!this.tripOffers[this.counter-1])
+    {
+      console.log(false);
+    }
+    else
+    {
+      this.displayedTripOffers.pop();
+      console.log(this.counter);
+      this.counter--;
+      this.displayedTripOffers.unshift(this.tripOffers[this.counter])
+    }
+  }
+
+  disablePreviousButton(): boolean {
+    if(this.counter == 0)
+      return true;
+    else
+      return false;
+  }
+
+
+
 
   employees: { image: string, firstName: string, lastName: string, description: string }[] = [
     { "image": "../../../assets/employee-photos/employee1.jpg","firstName": "John", "lastName": "Billy", "description": "I like trains" },
@@ -18,10 +122,21 @@ export class MainPageComponent implements OnInit {
     { "image": "../../../assets/employee-photos/employee3.jpg","firstName": "John", "lastName": "Billy", "description": "I like trains" },
   ];
 
+
   tripOffers: {image: string, tripDestination: string, tripDescription: string}[] = [
     { "image": "../../../assets/employee-photos/employee1.jpg","tripDestination": "John", "tripDescription": "Billy" },
     { "image": "../../../assets/employee-photos/employee2.jpg","tripDestination": "Bob", "tripDescription": "Krasinski" },
     { "image": "../../../assets/employee-photos/employee3.jpg","tripDestination": "John", "tripDescription": "Billy" },
+    { "image": "../../../assets/employee-photos/employee3.jpg","tripDestination": "John", "tripDescription": "Billy" },
+    { "image": "../../../assets/employee-photos/employee3.jpg","tripDestination": "John", "tripDescription": "Billy" },
+    { "image": "../../../assets/employee-photos/employee2.jpg","tripDestination": "Bob", "tripDescription": "Krasinski" },
+    { "image": "../../../assets/employee-photos/employee2.jpg","tripDestination": "Bob", "tripDescription": "Krasinski" },
+    { "image": "../../../assets/employee-photos/employee3.jpg","tripDestination": "John", "tripDescription": "Billy" },
+    { "image": "../../../assets/employee-photos/employee3.jpg","tripDestination": "John", "tripDescription": "Billy" },
+    { "image": "../../../assets/employee-photos/employee3.jpg","tripDestination": "John", "tripDescription": "Billy" },
+    { "image": "../../../assets/employee-photos/employee2.jpg","tripDestination": "Bob", "tripDescription": "Krasinski" },
   ]
+
+  displayedTripOffers: {image: string, tripDestination: string, tripDescription: string}[] = []
 
 }
